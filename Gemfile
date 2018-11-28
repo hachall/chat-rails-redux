1d7d088ed099cdcc54790c1884cd5ef31d766ab4
Hamish Hall <hachall@hotmail.com>
2018-11-28 15:07:51 +0000

    seed built

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'sassc-rails'
 gem 'simple_form'
 gem 'uglifier'
 gem 'webpacker'
+gem 'faker', :git => 'https://github.com/stympy/faker.git', :branch => 'master'
 
 group :development do
   gem 'web-console', '>= 3.3.0'
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,33 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require 'faker'
+
+
+Message.destroy_all
+User.destroy_all
+Channel.destroy_all
+
+names = %w(general paris react)
+nicknames = []
+6.times do
+  nicknames.push(Faker::Name.first_name)
+end
+# nicknames = %w(Papillard ssaunier monsieurpaillard krokrob Eschults)
+
+channels = names.map do |name|
+  Channel.create(name: name)
+end
+
+users = nicknames.map do |nickname|
+  User.create(email: "#{nickname.downcase}@lewagon.com", password: "testtest")
+end
+
+20.times do
+  Message.create! user: users.sample, channel: channels.sample, content: Faker::Hacker.say_something_smart
+end
+
+puts 'Channels:'
+channels.each do |channel|
+  puts "- #{channel.id}: #{channel.name}"
+end
