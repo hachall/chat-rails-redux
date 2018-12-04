de3b05dd967e6a39909c162e054ac70a0ed633ee
Hamish Hall <hachall@hotmail.com>
2018-12-04 11:43:25 +0000

    db nickname to message author

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_28_125935) do
+ActiveRecord::Schema.define(version: 2018_11_28_173925) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -27,6 +27,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_125935) do
     t.datetime "updated_at", null: false
     t.bigint "user_id"
     t.bigint "channel_id"
+    t.string "author"
     t.index ["channel_id"], name: "index_messages_on_channel_id"
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
@@ -39,6 +40,7 @@ ActiveRecord::Schema.define(version: 2018_11_28_125935) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "nickname"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
